a2b5fa4853b16b292a4277b1d5c70a8127491c4a
devCJH <http-@naver.com>
2023-05-09 15:56:43 +0900

    replyList

--- src/main/java/com/koreaIT/demo/service/ReplyService.java
+++ src/main/java/com/koreaIT/demo/service/ReplyService.java
@@ -1,10 +1,13 @@
 package com.koreaIT.demo.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.koreaIT.demo.repository.ReplyRepository;
 import com.koreaIT.demo.util.Util;
+import com.koreaIT.demo.vo.Reply;
 import com.koreaIT.demo.vo.ResultData;
 
 @Service
@@ -23,6 +26,10 @@ public class ReplyService {
 		int id = replyRepository.getLastInsertId();
 		
 		return ResultData.from("S-1", Util.f("%d번 댓글이 등록되었습니다", id), "id", id);
+	}
+
+	public List<Reply> getReplies(String relTypeCode, int relId) {
+		return replyRepository.getReplies(relTypeCode, relId);
 	}
 	
 }
