b0d32c7292c58d7b76514895fafb81eb51ae369c
devCJH <http-@naver.com>
2023-05-23 13:56:02 +0900

    loginPw sha256

--- src/main/java/com/koreaIT/demo/util/Util.java
+++ src/main/java/com/koreaIT/demo/util/Util.java
@@ -1,5 +1,7 @@
 package com.koreaIT.demo.util;
 
+import java.security.MessageDigest;
+
 public class Util {
 	public static boolean empty(Object obj) {
 		
@@ -56,6 +58,26 @@ public class Util {
 					location.replace('%s');
 				</script>
 				""", msg, uri);
+	}
+	
+	public static String sha256(String base) {
+		try {
+			MessageDigest digest = MessageDigest.getInstance("SHA-256");
+			byte[] hash = digest.digest(base.getBytes("UTF-8"));
+			StringBuffer hexString = new StringBuffer();
+
+			for (int i = 0; i < hash.length; i++) {
+				String hex = Integer.toHexString(0xff & hash[i]);
+				if (hex.length() == 1)
+					hexString.append('0');
+				hexString.append(hex);
+			}
+
+			return hexString.toString();
+
+		} catch (Exception ex) {
+			return "";
+		}
 	}
 	
 }
